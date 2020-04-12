0f9f361dd87c5a7b0609740cdd97b130b6dbea07
Ning Sun <sunng@protonmail.com>
2020-04-12 21:59:32 +0800

    (feat) add rate limit wrap function

--- src/jstackutils/core.clj
+++ src/jstackutils/core.clj
@@ -14,3 +14,14 @@
         out-file (io/file output-path file-full-name)]
     (let [output (:out (shell/sh "jstack" (str (pid/pid))))]
       (spit out-file output))))
+
+(defn with-min-interval [last-call-time-atom
+                         f
+                         min-interval-ms]
+  (when (> (- (System/currentTimeMillis) @last-call-time-atom)
+           min-interval-ms)
+    (locking last-call-time-atom
+      (when (> (- (System/currentTimeMillis) @last-call-time-atom)
+               min-interval-ms)
+        (f)
+        (reset! last-call-time-atom (System/currentTimeMillis))))))

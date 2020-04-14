2f781117c1224c62e2d95db2ca860a19206bf496
Ning Sun <sunng@protonmail.com>
2020-04-15 00:08:03 +0800

    (feat) add jstack output analysis functions

--- src/jstackutils/core.clj
+++ src/jstackutils/core.clj
@@ -1,6 +1,7 @@
 (ns jstackutils.core
   (:require [jstackutils.pid :as pid]
             [clojure.java.io :as io]
+            [clojure.string :as string]
             [clojure.java.shell :as shell])
   (:import [java.text SimpleDateFormat]
            [java.util Date]))
@@ -9,7 +10,28 @@
   (let [default-format (SimpleDateFormat. "yyyyMMddHHmmss")]
     (.format default-format (Date.))))
 
-(defn jstack [output-prefix]
+(defn- split-by-newline [jstack-output]
+  (string/split jstack-output #"\n"))
+
+(defn- join-by-newline [seqs]
+  (string/join seqs "\n"))
+
+(defn- split-by-double-newlines [jstack-output]
+  (string/split jstack-output #"\n\n"))
+
+(defn- join-by-double-newlines [seqs]
+  (string/join seqs "\n\n"))
+
+(defn- split-stack-trace [jstack-output]
+  (mapv split-by-newline (split-by-double-newlines jstack-output)))
+
+(defn- join-stack-trace [jstack-output-seqs]
+  (join-by-double-newlines (map join-by-newline jstack-output-seqs)))
+
+(defn- is-thread-stacktrace [jstack-output-seq-item]
+  (string/starts-with? (first jstack-output-seq-item) "\""))
+
+(defn jstack [output-prefix & {:keys [filters]}]
   (let [file-full-name (str output-prefix "-" (time-now))
         out-file (io/file file-full-name)]
     (let [output (:out (shell/sh "jstack" (str (pid/pid))))]
@@ -25,3 +47,8 @@
                min-interval-ms)
         (f)
         (reset! last-call-time-atom (System/currentTimeMillis))))))
+
+(defn thread-prefix-filter [thread-prefix]
+  (fn [jstack-output]
+    ;; TODO: impl
+    ))
